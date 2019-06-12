449cb130ee1bbf32e9cb45ce3ad9b99479bf1a7b
my <2575572904@qq.com>
2019-06-12 20:56:59 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/coolweather/my/coolweather/db/County.java
@@ -0,0 +1,18 @@
+package coolweather.my.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 25755 on 2019/6/12.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private String weatherId;
+    private int provinceId;
+
+
+
+}
---
+++ app/src/main/java/coolweather/my/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package coolweather.my.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 25755 on 2019/6/12.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
